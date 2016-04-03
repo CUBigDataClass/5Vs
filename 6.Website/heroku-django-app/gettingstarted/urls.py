1dd24307b7e89f1ed17545432cd709a5a3219b49
Bu Sun Kim <busunkim96@gmail.com>
2016-04-02 22:37:58 -0600

    added heroku django website

---
+++ 6.Website/heroku-django-app/gettingstarted/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+import hello.views
+
+# Examples:
+# url(r'^$', 'gettingstarted.views.home', name='home'),
+# url(r'^blog/', include('blog.urls')),
+
+urlpatterns = [
+    url(r'^$', hello.views.index, name='index'),
+    url(r'^index$', hello.views.index, name='index'),
+    url(r'^about$', hello.views.about, name="about"),
+    url(r'^db', hello.views.db, name='db'),
+    url(r'^admin/', include(admin.site.urls)),
+
+]
---
+++ 6.Website/heroku-django-app/hello/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Greeting
+
+# Create your views here.
+def index(request):
+    # return HttpResponse('Hello from Python!')
+    return render(request, 'site-home.html')
+
+
+def about(request):
+    return render(request, 'site-about.html')
+
+
+def db(request):
+
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, 'db.html', {'greetings': greetings})
+
