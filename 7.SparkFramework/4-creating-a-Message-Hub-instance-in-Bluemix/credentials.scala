13699a8a945e70132dd1ef1c9663db16b897139a
Afnan Aldhahri <afnan.aldhahri@colorado.edu>
2016-04-21 21:56:09 -0600

    Update credentials.scala

--- 7.SparkFramework/4-creating-a-Message-Hub-instance-in-Bluemix/credentials.scala
+++ 7.SparkFramework/4-creating-a-Message-Hub-instance-in-Bluemix/credentials.scala
@@ -1,7 +1,7 @@
 
-val demo = com.ibm.cds.spark.samples.MessageHubStreamingTwitter
+ val demo = com.ibm.cds.spark.samples.MessageHubStreamingTwitter
  val config = demo.getConfig()
- ...
+
  config.setConfig("bootstrap.servers","kafka01-prod01.messagehub.services.us-south.bluemix.net:9094,kafka02-prod01.messagehub.services.us-south.bluemix.net:9094,kafka03-prod01.messagehub.services.us-south.bluemix.net:9094,kafka04-prod01.messagehub.services.us-south.bluemix.net:9094,kafka05-prod01.messagehub.services.us-south.bluemix.net:9094")
   
  config.setConfig("api_key","XXXX")
